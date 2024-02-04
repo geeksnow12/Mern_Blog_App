760b4bf0506d8e67017018d7e7c6b4a4b653e032
Sneha Khandelwal <139537271+geeksnow12@users.noreply.github.com>
2024-02-04 14:50:19 +0530

    Add files via upload

---
+++ client/src/Post.js
@@ -0,0 +1,16 @@
+export default function Post(){
+    return(
+        <div className="post">
+      <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/10/F-150-Lightning-Flash-3.jpg?w=850&h=492&crop=1" alt="" />
+        </div>
+      <div className="texts">
+      <h2>Full-house battery</h2>
+      <p className="info">
+        <a className="author">Sneha Khandelwal</a>
+        <time>2023-12-12 11:11</time>
+      </p>
+      <p className="summary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis nulla architecto dolores, aspernatur ullam suscipit tempora sapiente eos ducimus adipisci?</p>
+      </div></div>
+    );
+}
---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,10 @@
+export default function CreatePost(){
+    return(
+        <form>
+            <input type="title" placeholder={'Title'}/>
+            <input type="summary" placeholder={'Summary'}/>
+            <input type="file" />
+            <Reactquilljs/>
+        </form>
+    );
+}
